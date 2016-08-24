abed3900bfda310e781522d4556a5f96edfae402
Daniel Heath <daniel@heath.cc>
2016-08-24 16:50:35 +1000

    Require auth for the admin section

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def require_basic_auth
+    request_http_basic_authentication unless authenticate_with_http_basic do |u, p|
+      u == "admin" && p == ENV['ADMIN_PASSWORD']
+    end
+  end
 end
