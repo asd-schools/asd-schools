9519bd2e1fe638c88343cba43ceaec340b5802d2
Daniel Heath <daniel@heath.cc>
2016-08-26 00:13:31 +1000

    Fix migration to clear school data

--- db/migrate/20160825015559_update_schools.rb
+++ db/migrate/20160825015559_update_schools.rb
@@ -1,5 +1,6 @@
 class UpdateSchools < ActiveRecord::Migration[5.0]
   def change
+    School.delete_all
     change_table :schools do |t|
       t.string :autism_characterstics, null: false, array: true, default: []
       t.string :sector, null: false
