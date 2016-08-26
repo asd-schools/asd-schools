a621ab54b1a0306e28fd024cc83abd413f6f1272
Grace Peng <grace.peng@rea-group.com>
2016-08-26 14:26:18 +1000

    oops syntax error

--- app/assets/javascripts/search_results_hide_form.js
+++ app/assets/javascripts/search_results_hide_form.js
@@ -1,6 +1,6 @@
 $(function onLoad() {
     if ($(document.body).hasClass('searches_controller')) {
-        if($('.card').length()==0) return;
+        if($('.card').length==0) return;
         $('body').addClass('search-collapsed');
         $('.expand-button').click(function(){
             $('body').removeClass('search-collapsed');
