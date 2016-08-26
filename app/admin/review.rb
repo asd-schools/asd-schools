beb8eea625170304849dae0d79d887ce87bb17ad
Daniel Heath <daniel@heath.cc>
2016-08-26 14:17:37 +1000

    Reference school in reviews admin

--- app/admin/review.rb
+++ app/admin/review.rb
@@ -1,3 +1,34 @@
 ActiveAdmin.register Review do
-  permit_params :published
+  permit_params :published, :comments
+
+  limit_length = lambda do |str|
+    if str.length > 30
+      str[0..30] + "..."
+    else
+      str
+    end
+  end
+
+  index do
+    selectable_column
+    id_column
+    column :school
+    column :review_type
+    column :published
+    column :created_at
+    column :pros do |review|
+      limit_length.call(review.pros)
+    end
+    column :cons do |review|
+      limit_length.call(review.cons)
+    end
+    column :comments do |review|
+      limit_length.call(review.comments)
+    end
+    column :score
+    column :child_characteristics
+    actions
+  end
+
 end
+
